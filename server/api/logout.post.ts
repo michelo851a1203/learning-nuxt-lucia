1e6fa5756514ae8f8a941174508e1b66a8958dc6
MichaelHo <michelo851a1203@gmail.com>
2024-05-12 17:41:48 +0800

    feat(): add log out method # 2024-05-12-005
    	新檔案：   server/api/logout.post.ts

---
+++ server/api/logout.post.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async event => {
+  if (!event.context.session) {
+    throw createError({
+      statusCode: 403
+    });
+  }
+  await lucia.invalidateSession(event.context.session.id);
+  appendHeader(event, 'Set-Cookie', lucia.createBlankSessionCookie().serialize());
+  return {
+    message: 'log out success'
+  };
+});
